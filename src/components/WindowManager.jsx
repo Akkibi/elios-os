ff3af29b40c2534102372d15ddb344df09417d16
Akkibi <akiravalade@gmail.com>
2024-01-26 11:32:03 +0100

    debug

--- src/components/WindowManager.jsx
+++ src/components/WindowManager.jsx
@@ -7,6 +7,14 @@ const WindowManager = () => {
   return (
     <div className="fixed inset-0 w-full h-full">
       <h1>Welcome</h1>
+      <div>
+        <h2>Documents</h2>
+        <ul>
+          {folderStructure.folders[0].content.map((file) => (
+            <li key={file}>{file}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
