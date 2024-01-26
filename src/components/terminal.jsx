a1ed65739ca2429d95cebcb2428842f954fb81a8
v5slv <valerie5slv@gmail.com>
2024-01-26 14:26:06 +0100

    updated terminal style

--- src/components/terminal.jsx
+++ src/components/terminal.jsx
@@ -8,9 +8,9 @@ const Terminal = () => {
                 <p>Welcome to the terminal ! <br /> You are currently connected to EU8. Explore, create, and manage your projects with care.</p>
 
 
-                <div className="flex" >
-                    <p>@EliOsTem </p>
-                    <input className="ml-4 h-1/2 w-full text-black p-3 focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-4 " type="text" name="" id="" />
+                <div className="flex items-center gap-2" >
+                    <p>@walt </p>
+                    <input className="h-1/2 w-full text-black p-3 focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-4 bg-transparent border-l-2 border-white" type="text" name="" id="" />
                 </div>
             </div>
 
