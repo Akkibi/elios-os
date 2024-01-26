160382b487ca0be869c6195d5598dfb622ed6342
Valentin Bleuse <bleusevalentin@orange.fr>
2024-01-26 14:08:28 +0100

    Co-authored-by: Valerie <v5slv@users.noreply.github.com>

--- src/components/terminal.jsx
+++ src/components/terminal.jsx
@@ -1,11 +1,21 @@
 const Terminal = () => {
 
     return (
-      <div className="w-full h-full bg bg-slate-950 ">
-        <input className="w-full" type="text" name="" id="" />
-        <p>Salut c'est le terminal</p>
-      </div>
+        <div className="w-full h-screen flex flex-col justify-start  ">
+
+            <div className=" w-2/3 flex flex-col sticky bottom-0 justify-start  mt-20 ml-20">
+                <h1 className="  text-4xl mb-4">Terminal</h1>
+                <p>Welcome to the terminal ! <br /> You are currently connected to EU8. Explore, create, and manage your projects with care.</p>
+
+
+                <div className="flex" >
+                    <p>@EliOsTem </p>
+                    <input className="ml-4 h-1/2 w-full text-black p-3 focus:outline-none focus:border-pink-500 focus:ring-pink-500 focus:ring-4 " type="text" name="" id="" />
+                </div>
+            </div>
+
+        </div>
     );
-  };
-  
-  export default Terminal;
+};
+
+export default Terminal;
