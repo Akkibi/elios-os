d087b62780d9a534ceda01c6be357d6efa00bb19
v5slv <valerie5slv@gmail.com>
2024-02-10 08:39:20 +0100

    Added colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
     extend: {
       fontFamily: {
         urbanist: ['Urbanist', 'sans-serif']
+      },
+      dropShadow: {
+        'sm': '0px 0px 53.33333206176758px rgba(0.7176470756530762,1,0.9490196108818054,0.12999999523162842)',
+        
       }
+    },
+    colors: {
+      'white': '#ffffff',
+      'midnight': '#05080D',
+      'blue': '#B7FFF2',
+      'red': '#FF0000',
+      transparent: 'transparent',
+      current: 'currentColor'
     },
   },
   plugins: [],
