d0d8ceb710ad163897ff9586acc96bd092aa8848
Akira Valade <akiravalade@gmail.com>
2024-06-02 04:58:08 +0200

    fix duplicate

--- src/components/terminal.jsx
+++
@@ -1,7 +0,0 @@
-const Terminal = () => {
-    return (
-            <p className="text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Terminal</p>
-    );
-}
-
-export default Terminal
