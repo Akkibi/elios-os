b44a98401b0e6d494fc5c3981f9b4744a1c00339
Akira Valade <akiravalade@gmail.com>
2024-06-02 17:17:16 +0200

    fix terminal empty promt issue, performance, and scroll auto

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,51 +1,62 @@
-    import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
-    const Terminal = () => {
-        const inputRef = useRef(null);
-        const [entries, setEntries] = useState([]);
-        const [input , setInput] = useState('');
+const Terminal = () => {
+    const inputRef = useRef(null);
+    const terminalRef = useRef(null);
+    const [entries, setEntries] = useState([]);
+    const [input , setInput] = useState('');
 
-        useEffect(() => {
-            const handleKeyPress = (event) => {
-                setInput(inputRef.current.value)
-                if (event.key === 'Enter') {
-                    console.log(entries, inputRef.current.value);
-                    setEntries([...entries, input]);
-                    setInput("");
-                    inputRef.current.value = "";
-                }
+    useEffect(() => {
+        const handleKeyPress = (event) => {
+            setInput(inputRef.current.value)
+            if (event.key === 'Enter') {
+                console.log(entries, inputRef.current.value);
+                setEntries([...entries, input]);
+                setInput("");
+                inputRef.current.value = "";
+                terminalRef.current.scrollTop = terminalRef.current.scrollHeight;
             }
-            inputRef.current.focus();
-            inputRef.current.addEventListener('keyup', handleKeyPress);
-        });
+        }
+        inputRef.current.focus();
+        inputRef.current.addEventListener('keyup', handleKeyPress);
+        return () => {
+            if (inputRef.current) {
+            inputRef.current.removeEventListener('keyup', handleKeyPress);
+        }
+        }
+    });
 
-        const ascii = [` ___  _     ___    ___   ___       ___   ___  `,
-            `| __|| |   |_ _|  / _ \\ / __|     / _ \\ / __|  `,
-            `| _| | |__  | |  | (_) |\\__ \\    | (_) |\\__ \\ `,
-            `|___||____||___|  \\___/ |___/     \\___/ |___/ `];
+    const ascii = [` ___  _     ___    ___   ___       ___   ___  `,
+        `| __|| |   |_ _|  / _ \\ / __|     / _ \\ / __|  `,
+        `| _| | |__  | |  | (_) |\\__ \\    | (_) |\\__ \\ `,
+        `|___||____||___|  \\___/ |___/     \\___/ |___/ `];
 
-        return (
-            <div className="w-full h-full overflow-y-scroll font-mono	">
-                <div className="whitespace-pre leading-4">
-                    <p>{ascii[0]}</p>
-                    <p>{ascii[1]}</p>
-                    <p>{ascii[2]}</p>
-                    <p>{ascii[3]}</p>
-                    <p>{ascii[4]}</p>
-                </div>
-                <p className="leading-7">OS: ELIOS OS v15.5.3 Enterprise beta</p>
-                {entries.map((entry, index) => (                   
-                    <div  key={index}>
-                     <p className="leading-7">C:/user/schneider/desktop:~$ {entry}</p>
-                     <p className='text-red'>elios-cli: {entry}: command not found</p>
-                    </div>
-                ))}
-                <div className="flex leading-7">
-                    <p className="pr-2 leading-7">C:/user/schneider/desktop:~$</p>
-                    <input ref={inputRef} type="text" className="leading-7 p-0 m-0 bg-transparent text-black w-full outline-none" />
-                </div>
+    return (
+        <div className="w-full h-full overflow-y-scroll font-mono" ref={terminalRef}>
+            <div className="whitespace-pre leading-4">
+                <p>{ascii[0]}</p>
+                <p>{ascii[1]}</p>
+                <p>{ascii[2]}</p>
+                <p>{ascii[3]}</p>
+                <p>{ascii[4]}</p>
             </div>
-        );
-    }
+            <p className="leading-7">OS: ELIOS OS v15.5.3 Enterprise beta</p>
+            {entries.map((entry, index) => ( 
+                entry === '' ? 
+                
+                <p key={index} className="leading-7">C:/user/schneider/desktop:~$ {entry}</p>
+                    :                  
+                <div  key={index}>
+                    <p className="leading-7">C:/user/schneider/desktop:~$ {entry}</p>
+                    <p className='text-red'>elios-cli: {entry}: command not found</p>
+                </div>
+            ))}
+            <div className="flex leading-7">
+                <p className="pr-2 leading-7">C:/user/schneider/desktop:~$</p>
+                <input ref={inputRef} type="text" className="leading-7 p-0 m-0 bg-transparent text-black w-full outline-none" />
+            </div>
+        </div>
+    );
+}
 
-    export default Terminal;
+export default Terminal;
